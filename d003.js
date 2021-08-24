e5f6f50916550a7a5b1aee06bea8c7459c8e1180
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-24 21:37:41 +0900

    add_d003.js

---
+++ d003.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let num = lines[0]
+  let line = []
+  for(let i=1;i<=9;i++){line.push(num*i)}
+  console.log(line.join(' '));
+});
