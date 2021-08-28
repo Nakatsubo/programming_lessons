1810d57f1930a508b4487275051b085cae5ebcea
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 18:10:59 +0900

    add_d010.js

---
+++ d010.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let line = lines
+  let str1 = line[0]
+  let str2 = line[1]
+  console.log(str1+'@'+str2)
+});
