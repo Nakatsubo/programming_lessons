edc507b62e5b75191fb138227ce83de34678914c
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 18:40:27 +0900

    add_d013.js

---
+++ d013.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let line = lines[0].split(/\s/).map(Number)
+  let result1 = Math.floor(line[0]/line[1])
+  let result2 = Math.floor(line[0]%line[1])
+  console.log(result1+' '+result2)
+});
