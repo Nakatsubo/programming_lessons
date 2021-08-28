83263dee33e0fb590a9b5d3108196455d14b2f42
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 21:17:00 +0900

    add_d015.js

---
+++ d015.js
@@ -0,0 +1,16 @@
+// d016
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let line = lines[0]
+  // 一度目の出力で変数はマイナスされている？
+  for(let i = line;i--;)console.log(i+1)
+});
