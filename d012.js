34ada5352dcf31d97b8d6b758d6013fb224d6178
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 18:29:50 +0900

    add_d012.js

---
+++ d012.js
@@ -0,0 +1,15 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+　let line = lines[0]
+  // Math.abs() -> 値の絶対値を求める
+  console.log(Math.abs(line))
+});
