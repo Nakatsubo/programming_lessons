7a0fd326846f2f7bcf300bc966d15e8898a0b23a
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 18:24:25 +0900

    add_d011.js

---
+++ d011.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let line = lines[0]
+  const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let result = str.indexOf(line)
+  console.log(result+1)
+});
