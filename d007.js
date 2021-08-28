6f64bf69d5b01683ab0e6db5da617a2eb398b9ec
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 16:17:31 +0900

    add_d007.js

---
+++ d007.js
@@ -0,0 +1,18 @@
+// d007
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let line = lines[0]
+  let result
+  console.log('*'.repeat(line))
+});
