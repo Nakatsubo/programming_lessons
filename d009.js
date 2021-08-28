b5818c8751569bcf33240aad7e970d240e495cd4
YusukeNakatsubo <nakatsubo@mbp.local>
2021-08-28 16:33:44 +0900

    add_d009.js

---
+++ d009.js
@@ -0,0 +1,15 @@
+// d009
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let line = lines[0].split(/\s/).map(Number)
+  console.log(line[1] - line[0])
+})
