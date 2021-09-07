4806ee3fb64359c8645e6f2013d629ef63c0ae62
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-09-07 22:07:04 +0900

    add_d026.js

---
+++ d026.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  // filter() -> 与えられた関数によって実装されたテストに合格したすべての配列からなる新しい配列を生成する。
+  console.log(lines.filter(x=> x==='no').length);
+});
