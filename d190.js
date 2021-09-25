e654560d8ffe6f06a79091a75034be8c676f7712
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-09-25 10:02:43 +0900

    add_d190.js

---
+++ d190.js
@@ -0,0 +1,15 @@
+// d190
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT = Number(lines);
+  console.log(INPUT*10);
+});
