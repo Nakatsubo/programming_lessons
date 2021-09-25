7c3755a8e062223390f679bed3fd309ef2576993
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-09-25 09:45:33 +0900

    add_d031.js

---
+++ d031.js
@@ -0,0 +1,15 @@
+// d031
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT = lines;
+  console.log(INPUT*60);
+});
