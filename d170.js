5578d82da36ffaddd1376cde2530cef734257f37
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-09-25 10:29:48 +0900

    add_d170.js

---
+++ d170.js
@@ -0,0 +1,16 @@
+// d170
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const NumA = lines[0];
+  const NumB = lines[1];
+  console.log(NumB * NumA);
+});
