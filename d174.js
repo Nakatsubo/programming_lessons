1370b76492b434930948c24f4a2ffad1e2853175
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-10-09 19:56:33 +0900

    add_d174.js

---
+++ d174.js
@@ -0,0 +1,15 @@
+// 174
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT = Number(lines);
+  console.log(INPUT * 1500);
+});
