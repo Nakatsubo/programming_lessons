6e8647660464a5a4e322126338c94321b30c8f35
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-10-09 20:21:27 +0900

    add_d156.js

---
+++ d156.js
@@ -0,0 +1,17 @@
+// d156
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        N      = Number(INPUTS[0]),
+        M      = Number(INPUTS[1]);
+  console.log(N * M);
+});
