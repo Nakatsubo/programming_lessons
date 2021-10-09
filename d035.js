c913c9e0a234ae535a74888ea3ac404712a8e0a0
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-10-09 16:51:10 +0900

    add_c035.js

---
+++ d035.js
@@ -0,0 +1,15 @@
+// d035
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const [YEAR, MONTH, DAY] = lines[0].split(/\s/).map(String);
+  console.log(`${YEAR}/${MONTH}/${DAY}`);
+});
