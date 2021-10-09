e3d08c572fabf7eb359a74205b5d685a1fdd2f4b
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-10-09 20:46:44 +0900

    add_d166.js

---
+++ d166.js
@@ -0,0 +1,14 @@
+// d166
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  console.log(Number(lines[0]) * 7);
+});
