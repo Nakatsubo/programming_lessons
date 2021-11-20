9c2eb3a613453c494cf4f35bf1c296b084449a42
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-11-20 22:52:22 +0900

    add_d179.js

---
+++ d179.js
@@ -0,0 +1,17 @@
+// d179
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        MEMBER = Number(INPUTS[0]),
+        AMOUNT = Number(INPUTS[1]);
+  console.log(AMOUNT % MEMBER);
+});
