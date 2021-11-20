e2cae20224ea771755d8c33d746ee326f01a0e0d
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-11-20 21:57:21 +0900

    add_c034.js

---
+++ c034.js
@@ -0,0 +1,21 @@
+// c034
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT = lines[0].split(/\s/);
+  if (INPUT[0] === 'x') {
+    INPUT[1] === '+' ? console.log(Number(INPUT[4]) - Number(INPUT[2])) : console.log(Number(INPUT[4]) + Number(INPUT[2]));
+  } else if (INPUT[2] === 'x') {
+    INPUT[1] === '+' ? console.log(Number(INPUT[4]) - Number(INPUT[1])) : console.log(Number(INPUT[0]) - Number(INPUT[4]));
+  } else if (INPUT[4] === 'x') {
+    INPUT[1] === '+' ? console.log(Number(INPUT[0]) + Number(INPUT[2])) : console.log(Number(INPUT[0]) - Number(INPUT[2]));
+  }
+});
