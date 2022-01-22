8f62f7d7e3ae473f9272f4ad5fcc6a53e487b141
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-01-22 15:21:06 +0900

    add_c039.js

---
+++ c039.js
@@ -0,0 +1,22 @@
+// c039
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT = lines[0].split(/\+/);
+  let result = 0;
+  for (let i = 0; i < INPUT.length; i += 1) {
+    let slashCount   = (INPUT[i].match(/\//g) || []).length,
+        bracketCount = (INPUT[i].match(/</g) || []).length,
+        tmpSum       = bracketCount * 10 + slashCount;
+    result += tmpSum;
+  }
+  console.log(result);
+});
