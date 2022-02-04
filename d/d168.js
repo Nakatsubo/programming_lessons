70f531363a77ffc094dc5b36ec34aa80de5e3640
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 20:36:19 +0900

    add_d168.js

---
+++ d/d168.js
@@ -0,0 +1,18 @@
+// d168
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        YEAR  = String(INPUTS[0]),
+        MONTH = String(INPUTS[1]),
+        DAY   = String(INPUTS[2]);
+  console.log(`${MONTH}/${DAY}/${YEAR}`);
+});
