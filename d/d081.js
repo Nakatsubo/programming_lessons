29e0d22e3d9a307ddfca4c8ebbefc2bdaf0a62be
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 18:53:57 +0900

    add_d081.js

---
+++ d/d081.js
@@ -0,0 +1,17 @@
+// d081
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        COUNT  = Number(INPUTS[0]),
+        [WIDTH, HEIGHT] = INPUTS[1].split(/\s/).map(Number);
+  console.log((WIDTH * HEIGHT) % COUNT);
+});
