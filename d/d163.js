da5226f5ffab6454e87219375cd24ddfc89c729f
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 18:24:32 +0900

    add_d163.js

---
+++ d/d163.js
@@ -0,0 +1,15 @@
+// d163
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const STRING_ARY = lines[0].split('').map(String);
+  for (let i = 0; i < STRING_ARY.length; i += 1) { console.log(STRING_ARY[i]); }
+});
