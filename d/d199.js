1f3c3d7658131e00416b9aacdb61f010d02bcbd4
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 21:02:09 +0900

    add_d199.js

---
+++ d/d199.js
@@ -0,0 +1,17 @@
+// d199
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        X = Number(INPUTS[0]),
+        Y = Number(INPUTS[1]);
+  console.log(X * Y);
+});
