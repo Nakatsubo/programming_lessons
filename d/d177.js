c89aeba22e2adc592d70d32d77e6617298dac513
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 20:31:59 +0900

    add_d177.js

---
+++ d/d177.js
@@ -0,0 +1,15 @@
+// d177
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const [PRICE, VOLUME] = lines[0].split(/\s/).map(Number);
+  console.log(PRICE * VOLUME);
+});
