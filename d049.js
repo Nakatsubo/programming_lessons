1b396cde48bea6c1b161e6b87bf5f722825b9184
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 11:33:34 +0900

    add_d049.js

---
+++ d049.js
@@ -0,0 +1,16 @@
+// d049
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const STRING = String(lines[0]),
+        RESULT = STRING.replace(/noaki/, '');
+  console.log(RESULT);
+});
