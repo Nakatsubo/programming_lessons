b6cab2f663a7d5b9d4e1deaeaf88f2bc6502fbba
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 15:45:56 +0900

    add_d067.js

---
+++ d/d067.js
@@ -0,0 +1,17 @@
+// d067
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const VALUE = Number(lines[0]);
+  let flag = 'OFF';
+  if (VALUE % 2 !== 0) flag = 'ON';
+  console.log(flag);
+});
