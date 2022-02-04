250b7a2fb3ff9cfd538b2780c86e3a449c7b8a5b
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 20:59:45 +0900

    add_d198.js

---
+++ d/d198.js
@@ -0,0 +1,17 @@
+// d198
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        X = String(INPUTS[0]),
+        Y = String(INPUTS[1]);
+  console.log(`${X}/${Y}`);
+});
