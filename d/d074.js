0e01c5a679e7e16efdede4fa52eb71df4536d9f0
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 17:00:02 +0900

    add_d074.js

---
+++ d/d074.js
@@ -0,0 +1,15 @@
+// d074
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const VALUE = Number(lines[0]);
+  VALUE >= 24 ? console.log(VALUE - 24) : console.log(VALUE);
+});
