172b52fcd54e1044a41a0af0e12f7a766669b5c2
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 16:30:11 +0900

    add_d070.js

---
+++ d/d070.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const [TOTAL, NOW] = lines[0].split(/\s/).map(Number);
+  console.log(TOTAL - NOW);
+});
