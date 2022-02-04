08c504d0b48bb2aa27b3012ad3868228b02aa999
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 19:06:27 +0900

    add_d132.js

---
+++ d/d132.js
@@ -0,0 +1,17 @@
+// d132
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        COUNT  = Number(INPUTS[0]),
+        STRING = String(INPUTS[1]);
+  console.log(STRING.repeat(COUNT));
+});
