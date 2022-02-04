0a8b3a1626248ce33892c663227d5a1b85537467
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 22:07:33 +0900

    add_d088.js

---
+++ d/d088.js
@@ -0,0 +1,15 @@
+// d088
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const [HIGH, LOW] = lines[0].split(/\s/).map(Number);
+  console.log(HIGH - LOW);
+});
