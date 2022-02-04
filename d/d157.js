fb0fda33cd36c6d5a71d7d2bc655f6fbbbf460d5
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-04 19:09:40 +0900

    add_d157.js

---
+++ d/d157.js
@@ -0,0 +1,15 @@
+// d157
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const NUMBER = Number(lines[0]);
+  console.log(Math.pow(2, NUMBER));
+});
