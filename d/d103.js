793421ace6d681bf8f0ae20c3934c59487a97182
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-05 12:20:59 +0900

    add_d103.js

---
+++ d/d103.js
@@ -0,0 +1,15 @@
+// d103
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const STRING = lines[0].split('').map(String);
+  console.log(STRING.reverse().join(''));
+});
