170001356def400587abe3a59dee96878c77b5ba
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-05 12:25:57 +0900

    add_d104.js

---
+++ d/d104.js
@@ -0,0 +1,16 @@
+// d104
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const VALUE = Number(lines[0]);
+  if (VALUE < 10) console.log(1000);
+  else console.log(150 * VALUE);
+});
