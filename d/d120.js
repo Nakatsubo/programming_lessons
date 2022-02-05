a120433a30a19f1ecd3338d7f7449be4468b80e5
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-05 14:12:38 +0900

    add_d120.js

---
+++ d/d120.js
@@ -0,0 +1,15 @@
+// d120
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const VALUE = Number(lines[0]);
+  console.log(12 * VALUE);
+});
