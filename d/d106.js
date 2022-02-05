7a2e286ddd4dc6b40fbd7fa142164092de52bc6c
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-05 12:46:06 +0900

    add_d106.js

---
+++ d/d106.js
@@ -0,0 +1,16 @@
+// d106
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const X = Number(lines[0]),
+        Y = Number(lines[1]);
+  console.log(X % Y);
+});
