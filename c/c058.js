6a14ec19ecfa2054ad258ee3f64abf18a1bae9c7
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-07 19:36:17 +0900

    add_c058.js

---
+++ c/c058.js
@@ -0,0 +1,25 @@
+// c058
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT = lines;
+  let [count, correctStr, defaultStr] = INPUT[0].split(/\s/);
+  let result = 0;
+  while (true) {
+    if (correctStr !== defaultStr) {
+      correctStr = correctStr.substr(- 1) + correctStr.substr(0, count - 1);
+      result += 1;
+    } else {
+      console.log(result);
+      break;
+    }
+  }
+})
