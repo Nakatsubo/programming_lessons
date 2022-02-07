4690a68a2f7cf835c1606e65a40305831285e536
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-07 18:58:49 +0900

    add_c052.js

---
+++ c/c052.js
@@ -0,0 +1,17 @@
+// c052
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS                   = lines,
+        [HEIGHT, WIDTH]          = INPUTS[0].split(/\s/).map(Number);
+        [X_POSITION, Y_POSITION] = INPUTS[1].split(/\s/).map(Number);
+  console.log((WIDTH * Math.abs(X_POSITION)) + (HEIGHT * Math.abs(Y_POSITION)) - (Math.abs(X_POSITION) * Math.abs(Y_POSITION)));
+});
