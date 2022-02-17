5027c9097f1caaee7937dfa06131e50f1f777299
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-17 20:39:57 +0900

    add_b011

---
+++ b/b011.js
@@ -0,0 +1,20 @@
+// b011
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const [POCKET, CARD_NUMBER] = lines[0].split(/\s/).map(Number);
+  
+  const PAGE_NUMBER     = Math.floor((CARD_NUMBER - 1) / (POCKET * 2)) + 1,
+        PAGE_MAX_NUMBER = PAGE_NUMBER * POCKET * 2,
+        PAGE_MIN_NUMBER = PAGE_MAX_NUMBER - (POCKET * 2 - 1);
+        
+  console.log(PAGE_MAX_NUMBER + PAGE_MIN_NUMBER - CARD_NUMBER);
+});
