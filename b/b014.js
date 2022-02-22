124bd824a29a421c37a282861cd5714df893b11c
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-23 00:16:39 +0900

    add_d014.js

---
+++ b/b014.js
@@ -0,0 +1,42 @@
+// b014
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+const INPUTS = lines,
+[X, Y, Z] = INPUTS[0].split(/\s/).map(Number);
+
+// リスト
+let cubicArr = Array.from(new Array(Z), () => new Array(Y).fill('.')),
+    zCounter = 0,
+    yCounter = 0;
+
+// 標準入力
+let tmpCubicDataArr = [];
+for (let i = 0; i < INPUTS.length - 1; i += 1) {
+  let tmp = INPUTS[i + 1].split('');
+  if (tmp[0] !== '-') tmpCubicDataArr.push(tmp);
+}
+
+let newCubicArr = [];
+// リストを更新
+for (let i = 0; i < tmpCubicDataArr.length; i += 1) {
+// どうやって標準入力を入れるか
+  for (j = 0; j < Y; j += 1) {
+    let tmpArr = tmpCubicDataArr[i]; // i行目を入力
+    let tmpValue = tmpArr[j];
+    if (tmpValue === '#') {
+      cubicArr[i][j] = '#'; // ここが仕様上アクセスできずエラー -> スタック(Javascriptはあきらめた)
+    }
+  }
+}
+console.log(cubicArr);
+
+});
