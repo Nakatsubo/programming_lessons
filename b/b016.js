81f57e8a23a8cd00038380dfb12c9f53c7eeffc5
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-27 15:38:25 +0900

    add_b016.js

--- b/b016.js
+++ b/b016.js
@@ -12,22 +12,25 @@ reader.on('line', (line) => {
 reader.on('close', () => {
   const INPUTS = lines,
         [WIDTH, HEIGHT, COUNT] = INPUTS[0].split(/\s/).map(Number);
-
+        
   let [xPosition, yPosition] = INPUTS[1].split(/\s/).map(Number);
+      
   for (let i = 0; i < COUNT; i += 1) {
     let [direction, step] = INPUTS[i + 2].split(/\s/);
+    step = Number(step);
+    
     if (direction === 'U') {
-      if (yPosition + Number(step) % HEIGHT < HEIGHT) yPosition += Number(step) % HEIGHT;
-      else yPosition += Number(step) % HEIGHT - HEIGHT;
+      if (yPosition + step % HEIGHT < HEIGHT) yPosition += step % HEIGHT;
+      else yPosition += step % HEIGHT - HEIGHT;
     } else if (direction === 'D') {
-      if (yPosition - Number(step) % HEIGHT >= 0) yPosition -= Number(step) % HEIGHT;
-      else yPosition = HEIGHT - Math.abs(yPosition - Number(step) % HEIGHT);
+      if (yPosition - step % HEIGHT >= 0) yPosition -= step % HEIGHT;
+      else yPosition = HEIGHT - Math.abs(yPosition - step % HEIGHT);
     } else if (direction === 'R') {
-      if (xPosition + Number(step) % WIDTH < WIDTH) xPosition += Number(step) % WIDTH;
-      else xPosition += Number(step) % WIDTH - WIDTH;
+      if (xPosition + step % WIDTH < WIDTH) xPosition += step % WIDTH;
+      else xPosition += step % WIDTH - WIDTH;
     } else if (direction === 'L') {
-      if (xPosition - Number(step) % WIDTH >= 0) xPosition -= Number(step) % WIDTH;
-      else xPosition = WIDTH - Math.abs(xPosition - Number(step) % WIDTH);
+      if (xPosition - step % WIDTH >= 0) xPosition -= step % WIDTH;
+      else xPosition = WIDTH - Math.abs(xPosition - step % WIDTH);
     }
   }
 
