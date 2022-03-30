21aeff127673958c2579ec463db7c004417a0f98
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-30 23:21:52 +0900

    add_c106.js

---
+++ c/c106.js
@@ -0,0 +1,33 @@
+// c106
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        COUNT  = Number(INPUTS[0]),
+        SCORE_ARR = INPUTS[1].split(/\s/).map(Number);
+
+  let resultArr = Array(COUNT).fill(0);
+
+  for (let i = 0; i < COUNT; i += 1) {
+    let counter = 1;
+    for (let j = 0; j < COUNT; j += 1) {
+      if (SCORE_ARR[i] < SCORE_ARR[j]) counter += 1;
+    }
+    resultArr[i] = counter;
+  }
+
+  for (let i = 0; i < COUNT; i += 1) {
+    if (resultArr[i] === 1) console.log('G');
+    else if (resultArr[i] === 2) console.log('S');
+    else if (resultArr[i] === 3) console.log('B');
+    else console.log('N');
+  }
+});
