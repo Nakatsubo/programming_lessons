cc4ed5ef6a5d831da7f9fe0a20be21bb775dffde
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-04-26 23:33:59 +0900

    pending...

--- a/a052.js
+++ a/a052.js
@@ -13,38 +13,41 @@ reader.on('line', (line) => {
 reader.on('close', () => {
   const DATA   = lines,
         COUNT  = Number(DATA[0]),
-        [A, B] = DATA[1].split(/\s/).map(Number);
+        [A, B] = DATA[1].split(/\s/).map(Number),
+        AB     = A + B;
 
   let resultArr = Array(COUNT).fill(0);
-  let tmpArr = [-1];
+  // let tmpArr = [-1];
+  let aArr = Array(COUNT).fill(0),
+      bArr = Array(COUNT).fill(0);
 
-  while (true) {
-    let next = [];
+  // while (true) {
+  //   let next = [];
 
-    for (let i = 0; i < tmpArr.length; i += 1) {
-      if (i + A <= COUNT - 1) {
-        if (resultArr[i + A] === 0) {
-          next.push(i + A);
-          resultArr[i + A] = 1;
-        }
-        else {
-          resultArr[COUNT - 1] = 1;
-        }
-      }
-      if (i + B <= COUNT - 1) {
-        if (resultArr[i + B] === 0) {
-          next.push(i + B);
-          resultArr[i + B] = 1;
-        }
-        else {
-          resultArr[COUNT - 1] = 1;
-        }
-      }
-    }
+  //   for (let i = 0; i < tmpArr.length; i += 1) {
+  //     if (i + A <= COUNT - 1) {
+  //       if (resultArr[i + A] === 0) {
+  //         next.push(i + A);
+  //         resultArr[i + A] = 1;
+  //       }
+  //       else {
+  //         resultArr[COUNT - 1] = 1;
+  //       }
+  //     }
+  //     if (i + B <= COUNT - 1) {
+  //       if (resultArr[i + B] === 0) {
+  //         next.push(i + B);
+  //         resultArr[i + B] = 1;
+  //       }
+  //       else {
+  //         resultArr[COUNT - 1] = 1;
+  //       }
+  //     }
+  //   }
 
-    if (next.length === 0) break;
-    tmpArr = next;
-  }
+  //   if (next.length === 0) break;
+  //   tmpArr = next;
+  // }
   
   console.log(resultArr);
 });
